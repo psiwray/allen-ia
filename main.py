de2dde3503f9459398e4df872c57ab5da6a7b91d
psiwray <development@psiwray.com>
2021-02-07 11:07:06 +0100

    Added command line functionality.

--- main.py
+++ main.py
@@ -1,14 +1,45 @@
+from getopt import getopt
+from sys import argv
+from typing import Optional
+from os import mkdir
+
 from allen.input.inverse_relationships_table import read_inverse_relationships_table
 from allen.input.ternary_constraints_table import read_ternary_constraints_table
 from allen.input.time_intervals_table import read_time_intervals_table
 from allen.output.sat_output import generate_sat_output, Data, Coding
 
 if __name__ == "__main__":
+    matched_args, remaining_args = getopt(argv[1:], "d:c:o:", ["data=", "coding=", "output="])
+
+
+    def find_argument(short: str, long: str) -> Optional[str]:
+        for arg_name, arg_value in matched_args:
+            if arg_name == short or arg_name == long:
+                return arg_value
+
+        return None
+
+
+    data_folder = find_argument("-d", "--data") or "."
+    coding = find_argument("-c", "--coding") or "ternary_impl"
+    output_folder = find_argument("-o", "--output") or "."
+    input_file = remaining_args[0]
+
     data = Data(
-        read_inverse_relationships_table("data/inverse_relationships_table.txt"),
-        read_ternary_constraints_table("data/ternary_constraints_table.txt")
+        read_inverse_relationships_table(f"{data_folder}/inverse_relationships_table.txt"),
+        read_ternary_constraints_table(f"{data_folder}/ternary_constraints_table.txt")
     )
-    for group in read_time_intervals_table("data/time_intervals/3.txt"):
-        print(f"Clauses for group at index {group.number}:")
-        for line in generate_sat_output(group, data, Coding.TERNARY_IMPLICATION):
-            print(line)
+
+    coding_enum = ""
+    if coding == "ternary_impl":
+        coding_enum = Coding.TERNARY_IMPLICATION
+    elif coding == "expression_ref":
+        coding_enum = Coding.EXPRESSION_REFERENCE
+
+    for group in read_time_intervals_table(input_file):
+        mkdir(output_folder)
+
+        with open(f"{output_folder}/{group.number}.txt", "w") as file:
+            for line in generate_sat_output(group, data, coding_enum):
+                file.write(line)
+                file.write("\n")
